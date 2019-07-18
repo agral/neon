b4a32a4a8c10946285dc5a10ca9331c549e4be74
agral <adam@gralin.ski>
2019-07-19 00:51:37 +0200

    Implement setters for Vec2D's fields
    Implements the setter methods for Vec2D's fields:
     * setX()
     * setY()
     * setXY()
    
    With this implementation all the tests are passing.

--- src/Vec2D.hpp
+++ src/Vec2D.hpp
@@ -17,9 +17,9 @@ class Vec2D
  public:
   double x() const { return m_x; }
   double y() const { return m_y; }
-  void setX(double newX) { m_x = -1; } // TODO implement
-  void setY(double newY) { m_y = -1; } // TODO implement
-  void setXY(double newX, double newY) { m_x = -1; m_y = -1; } // TODO implement
+  void setX(double newX) { m_x = newX; }
+  void setY(double newY) { m_y = newY; }
+  void setXY(double newX, double newY) { m_x = newX; m_y = newY; }
 
  private:
   double m_x;
