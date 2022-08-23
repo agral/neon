7d248e8d58867224c0700e28e66a8ce074f4f995
agral <adam@gralin.ski>
2022-08-23 12:29:30 +0200

    Finish reimplementing Timer test suite in gtest
    Adds a final test for accurate reporting of the elapsed time.

--- test/test_Timer.cpp
+++ test/test_Timer.cpp
@@ -31,34 +31,31 @@ TEST(TimerTest, TimerCanBeStartedAndStopped) {
   EXPECT_FALSE(t.isStarted());
 }
 
-/*
-TEST_CASE("Timer class correctly implements its features")
-{
+TEST(TimerTest, TimerAccuratelyReportsElapsedTime) {
+  // Given a Timer,
+  Timer t{};
 
-  SECTION("Timer accurately reports the elapsed time")
-  {
-    ::neon::Timer t;
-    std::chrono::time_point<neon::Clock> beforeStart = neon::Clock::now();
-    t.start();
-    std::chrono::time_point<neon::Clock> afterStart = neon::Clock::now();
+  // when it is started and then, after some period of time, stopped,
+  auto beforeStart = neon::Clock::now();
+  t.start();
+  auto afterStart = neon::Clock::now();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+  std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
-    std::chrono::time_point<neon::Clock> beforeMeas = neon::Clock::now();
-    double durT = t.time();
-    std::chrono::time_point<neon::Clock> afterMeas = neon::Clock::now();
-    t.stop();
+  auto beforeMeasurement = neon::Clock::now();
+  double durationMeasured = t.time();
+  auto afterMeasurement = neon::Clock::now();
 
-    double dur1 = std::chrono::duration_cast<std::chrono::nanoseconds>(afterMeas - beforeStart).count() / 1000000000.0;
-    double dur2 = std::chrono::duration_cast<std::chrono::nanoseconds>(beforeMeas - afterStart).count() / 1000000000.0;
+  // then:
+  // - the time elapsed from just before turning on the Timer instance until just after taking the measurement
+  //   should be greater than or equal to the duration measured by the Timer instance
+  double duration1 = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      afterMeasurement - beforeStart).count() / 1'000'000'000.0;
+  EXPECT_TRUE(duration1 >= durationMeasured);
 
-    // dur1 is the time elapsed from before turning on the Timer until just after taking the measurement.
-    // This implies that dur1 should be greater than or equal to durT measured by the Timer.
-    CHECK(dur1 >= durT);
-
-    // dur2 is the time elapsed from after turning on the Timer until just before taking the measurement.
-    // This implies that dur2 should be equal to or less than durT measured by the Timer.
-    CHECK(dur2 <= durT);
-  }
+  // - the time elapsed from just after turning on the Timer instance until just before taking the measurement
+  //   should be smaller than or equal to the duration measured by the Timer instance
+  double duration2 = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      beforeMeasurement - afterStart).count() / 1'000'000'000.0;
+  EXPECT_TRUE(duration2 <= durationMeasured);
 }
-*/
